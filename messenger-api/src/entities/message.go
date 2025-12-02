5e0bb9058301c2626786fd48a35ed67ac0898224
GabryelleSS <gabryelle-gaby@hotmail.com>
2025-12-02 14:34:09 -0300

    feat: create Message entities

---
+++ messenger-api/src/entities/message.go
@@ -0,0 +1,14 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message struct {
+	Id uuid.UUID `json:"id"`
+	Content string `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	TimesSent int32 `json:"times_sent"`
+}
