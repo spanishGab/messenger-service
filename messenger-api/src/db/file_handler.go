962b9fb7371fd4d64af50f28b55ce6ea81bf1700
GabryelleSS <gabryelle-gaby@hotmail.com>
2025-12-12 14:49:44 -0300

    fix: update error messages in FileHandler methods to use English

--- messenger-api/src/db/file_handler.go
+++ messenger-api/src/db/file_handler.go
@@ -29,7 +29,7 @@ func (fh *FileHandler) Connection() {
 func (fh *FileHandler) Read() ([]byte, error) {
 	file, err := os.ReadFile(fh.FileName)
 	if err != nil {
-		return nil, fmt.Errorf("não foi possivel ler o arquivo %s", err.Error())
+		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
 	return file, nil
 }
@@ -37,7 +37,7 @@ func (fh *FileHandler) Read() ([]byte, error) {
 func (fh *FileHandler) Write(data []byte) (int, error) {
 	err := os.WriteFile(fh.FileName, data, OwnerRWPermission)
 	if err != nil { 
-		return 0, fmt.Errorf("não foi possivel escrever no arquivo %s", err.Error())
+		return 0, fmt.Errorf("failed to write to file: %w", err)
 	}
 	return len(data), nil
 }
