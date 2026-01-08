4a8e4e56085399d2656cdf2d44908f381f8097b6
GabryelleSS <gabryelle-gaby@hotmail.com>
2026-01-08 14:05:23 -0300

    fix: change enum to uppercase letters

--- messenger-api/src/handlers/command.go
+++ messenger-api/src/handlers/command.go
@@ -3,11 +3,17 @@ package handlers
 type CommandType string 
 
 const (
-	Create CommandType = "create"
-	List CommandType = "list"
-	Update CommandType = "update"
-	Delete CommandType = "delete"
+	Create CommandType = "CREATE"
+	List CommandType = "LIST"
+	ListByFilters CommandType = "LIST_BY_FILTERS"
+	Update CommandType = "UPDATE"
+	Delete CommandType = "DELETE"
 )
+
+type DateRangeDTO struct {
+	Start string
+	End  string
+}
 
 type Command struct {
 	Type CommandType
