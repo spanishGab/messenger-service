91aee62585fe0be328ca6d26c81fb2a557e76821
GabryelleSS <gabryelle-gaby@hotmail.com>
2026-04-24 08:24:40 -0300

    feat: implement command type validation function

---
+++ messenger-api/src/cmd/validator.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+	"messenger-api/src/handlers"
+)
+
+func ValidateCommandType(input []string) (*handlers.CommandType, error) {
+	if len(input) < 2 {
+		return nil, fmt.Errorf("error - invalid command")
+	}
+
+	commandType := handlers.CommandType(input[1])
+	if _, ok := handlers.Commands[commandType]; !ok {
+		return nil, fmt.Errorf("error - invalid command")
+	}
+
+	return &commandType, nil
+}
